9218f7c7d1c8b8b084778332cfe66587d343612b
Jack Bowden <jabowden@email.wm.edu>
2018-09-18 13:48:43 -0400

    Added gameLogic

--- src/applicationLayer.cpp
+++ src/applicationLayer.cpp
@@ -2,10 +2,13 @@
 
 #include "viewPlayer.h"
 #include "viewAI.h"
+#include "gameLogic.h"
+//! DO I NEED DESTRUCTORS AND IF SO, WHERE?
 
 int main(int argc, char** argv)
 {
   // create main window and prevent it from being resized
+  // this is my init
   sf::RenderWindow App(sf::VideoMode(800,600,32), "Pong, by Jack Bowden", sf::Style::Close);
 
   // start main loop
@@ -16,6 +19,11 @@ int main(int argc, char** argv)
     while(App.pollEvent(Event))
     {
       // Exit
+      // this is my shutdown
+
+      //need to start looping to accept input here i suppose
+
+
       if(Event.type == sf::Event::Closed)
         App.close();
     }
